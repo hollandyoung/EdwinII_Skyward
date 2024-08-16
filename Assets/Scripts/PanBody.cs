e143f75ec87699c6bf581d6459e67cca0f8d7e90
Holland Young <hollandkariyoung@gmail.com>
2024-08-16 12:03:07 -0700

    Mockup of house prefab, added preliminary pan body script (unfinished)

---
+++ Assets/Scripts/PanBody.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanBody : MonoBehaviour
+{
+    // Variables
+    [SerializeField] GameObject slot1;
+    [SerializeField] GameObject slot2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
