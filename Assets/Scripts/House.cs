c4cb986af7d233eb6332fd6211d6bb52c28c3553
Tareyal <alextodd2008@gmail.com>
2024-08-16 15:43:22 -0700

    Bricks have been invented!

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
+    public GameObject Bricker;
+    private Bricks Brick;
+
+
     // Variables
     private string houseType;
 
     public string GetHouseType()
     {
         return houseType;
+    }
+    void Start()
+    {
+        Brick = Bricker.GetComponent<Bricks>();
+        StartCoroutine(ProduceBricks());
+    }
+
+    IEnumerator ProduceBricks()
+    {
+       //Wait for 4 seconds
+        yield return new WaitForSeconds(4);
+        Brick.AddBrickCount();
     }
 }
