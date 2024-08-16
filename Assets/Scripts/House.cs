fa2b1549317bd5cc36040ec95cc75cb12cf392ce
Holland Young <hollandkariyoung@gmail.com>
2024-08-16 13:08:09 -0700

    (WIP) Edits to PanBody, added House script

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    // Variables
+    private string houseType;
+
+    public string getType()
+    {
+        return houseType;
+    }
+}
