3dedff38804229d80a2c1e9360ade354b3bc3196
Colin Cerise <colin.m.cerise@gmail.com>
2024-08-16 16:53:12 -0700

    Made start button appear on Start and added UI Manager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] GameObject restartButton;
+    [SerializeField] GameObject startButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startButton.SetActive(true);
+        restartButton.SetActive(false);
     }
 
     // Update is called once per frame
