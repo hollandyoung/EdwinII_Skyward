323ad31234e0520cfba9bc1d667534c343c1ffdc
Colin Cerise <colin.m.cerise@gmail.com>
2024-08-16 18:06:17 -0700

    Got the restart button to fully work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        uIManagerScript = uIManagerObject.GetComponent<UIManager>();
         gameActive = false;
     }
 
