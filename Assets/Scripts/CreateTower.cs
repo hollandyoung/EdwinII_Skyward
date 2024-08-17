528ab00bdf59ac846a62974e3636ecd9f26fe68e
Tareyal <alextodd2008@gmail.com>
2024-08-16 17:02:07 -0700

    We must erect the tower

---
+++ Assets/Scripts/CreateTower.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTower : MonoBehaviour
+{
+    public GameObject TowerPrefab;
+    public bool MakeTower = true;
+    public float centerX = 0;
+    public float StartY = 0;
+    public float Yscaling = 10;
+    public float NumberOfSegments = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TowerTest());
+    }
+
+    IEnumerator TowerTest()
+    {
+        while (MakeTower) {
+       //Wait for 1 seconds
+        yield return new WaitForSeconds(1);
+        GenerateTower();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GenerateTower()
+    {
+        //a = new gameObject Instantiate(TowerPrefab);
+        Instantiate(TowerPrefab, new Vector2(centerX, StartY + (Yscaling * NumberOfSegments)));
+        NumberOfSegments += 1;
+    }
+}
