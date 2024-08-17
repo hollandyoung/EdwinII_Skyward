ddc1b7105348404ccad02607cd6b1b250c002f4d
Holland Young <hollandkariyoung@gmail.com>
2024-08-17 16:36:41 -0700

    Working on endgame collision

--- Assets/Scripts/Scale/PanBody.cs
+++ Assets/Scripts/Scale/PanBody.cs
@@ -15,25 +15,23 @@ public class PanBody : MonoBehaviour
         gMScript = gM.GetComponent<GameManager>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    /*void OnTriggerEnter2D(Collision2D collision)
     {
         if(collision.gameObject.layer == 7)
         {
             gMScript.EndGame();
         }
         Debug.Log(collision.gameObject.layer);
-    }
+    }*/
 
-    // NEED TO FIX PLEASE
-    #region
-    public float GetWeight()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        float weight = 0f;
-        /*for (int i = 0; i < positions.Length; i++)
+        if (collision.gameObject.layer == 7)
         {
-            weight += positions[i].GetComponentInChildren<HouseStack>().GetWeight();
-        }*/
-        return weight;
+            Debug.Log("Game Over");
+            gMScript.EndGame();
+        }
+
+        Debug.Log("Got here at least");
     }
-    #endregion
 }
