92d2f53f78266c1a94beaee6be552780ca005a6a
Tareyal <alextodd2008@gmail.com>
2024-08-17 16:57:59 -0700

    Added brick counter and display message for those who are broke

--- Assets/Scripts/Scale/Bricks.cs
+++ Assets/Scripts/Scale/Bricks.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Bricks : MonoBehaviour
 {
+
+    public TextMeshProUGUI BrickCounter;
+    
+    
     public float BrickCount = 70.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //BrickCounter = Text.Find("Brick Counter");
     }
 
     // Update is called once per frame
@@ -23,9 +29,14 @@ public class Bricks : MonoBehaviour
     public void AddBrickCount()
     {
         BrickCount += 1;
+        BrickCounter.text = ("You Have: " + BrickCount + " Bricks");
     }
     public void SetBrickCount(float BrickSet)
     {
         BrickCount = BrickSet;
+        BrickCounter.text = ("You Have: " + BrickCount + " Bricks");
+        if (BrickCount == 0) {
+            ("You are broke");
+        }
     }
 }
