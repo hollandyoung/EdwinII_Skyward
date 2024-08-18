fdba718be9d23ff08ee22065450dc99bd88719da
Tareyal <alextodd2008@gmail.com>
2024-08-17 18:17:22 -0700

    The edwin hitman is now doing his job

--- Assets/Scripts/Management/CullingEdwin.cs
+++ Assets/Scripts/Management/CullingEdwin.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CullingEdwin : MonoBehaviour
 {
+    public GameObject EdwinCreator;
+    private CreateTower CrTower;
+    private float Segments;
+
+    public float DeLoadGap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EdwinCreator = GameObject.Find("Edwin Manager");
+        CrTower = EdwinCreator.GetComponent<CreateTower>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Segments = CrTower.GetNumberOfSegments();
+        if (Segments - (this.transform.position.y / .8) > DeLoadGap) {
+            Destroy(this.gameObject);
+        }
     }
 }
